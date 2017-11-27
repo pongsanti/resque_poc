88dbc78489b79bbc5e832842d4ca4530b7d11e97
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-11-28 01:20:43 +0700

    fix model missing require

--- lib/task/update_status.rb
+++ lib/task/update_status.rb
@@ -3,6 +3,7 @@ module UpdateStatus
   
   def self.perform post_id
     require_relative '../db/connection'
+    require_relative '../db/post'
     post = Post[post_id]
     sleep 15
     post.update(status: 'SUCCESS')
