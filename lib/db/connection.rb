a5b69380c53d24e089819bfaaf4c91605c15f3a9
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-11-28 12:43:20 +0700

    disable logger because of error

--- lib/db/connection.rb
+++ lib/db/connection.rb
@@ -1,14 +1,14 @@
 require 'dotenv/load'
 require 'sequel'
-require 'logger'
+#require 'logger'
 
 HOST = ENV['DB_HOST']
 USER = ENV['DB_USER']
 PASS = ENV['DB_PASS']
 DBNAME = ENV['DB_NAME']
 
-LOG = Logger.new(STDOUT)
+#LOG = Logger.new(STDOUT)
 
 DB = Sequel.connect("postgres://#{USER}:#{PASS}@#{HOST}/#{DBNAME}",
   max_connections: 10)
-DB.loggers << LOG
+#DB.loggers << LOG
